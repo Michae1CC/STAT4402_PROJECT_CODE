c23868d11d93117918ddf84606b104a369d91aad
Michae1 <333mmcc@gmail.com>
2020-10-09 10:45:23 +1000

    changes to save mass spec data

--- src_data/extract_mass_spec.py
+++ src_data/extract_mass_spec.py
@@ -6,45 +6,63 @@ import pandas as pd
 from pprint import pprint
 from jcamp import JCAMP_reader
 
-import matplotlib.pyplot as plt
 
-MASS_SPEC_PATH = os.path.join('data', 'mass')
+def create_mass_spec_df(mass_spec_path, x_max_bin=501):
+    """
+    Creates a new data frame for mass spec data.
 
-x_bins = np.arange(0, 501, 1)
-main_df = pd.DataFrame({'x': pd.cut(np.arange(1, 499, 50), x_bins)})
-main_df.set_index('x', inplace=True)
+    Parameters:
+        mass_spec_path:
+            A path to where all the mass spec jdx files are stored.
 
-for jdx_file in os.listdir(os.path.join('data', 'mass'))[:2]:
+        x_max_bin:
+            The maximum mass spec value to be captured.
 
-    if not jdx_file.endswith('.jdx'):
-        continue
+    Return:
+        Returns a dataframe where the indexes are mass spec buckets and new
+        columns represent new columns.
+    """
 
-    jcamp_dict = JCAMP_reader(os.path.join(MASS_SPEC_PATH, jdx_file))
+    x_bins = np.arange(0, x_max_bin, 1)
+    main_df = pd.DataFrame({'x': pd.cut(np.arange(1, 499, 50), x_bins)})
+    main_df.set_index('x', inplace=True)
 
-    x_values = jcamp_dict['x'] * float(jcamp_dict['xfactor'])
-    y_values = jcamp_dict['y'] * float(jcamp_dict['yfactor'])
+    for jdx_file in os.listdir(mass_spec_path):
 
-    # Scale by largest y val
-    y_values = y_values / max(y_values)
+        if not jdx_file.endswith('.jdx'):
+            continue
 
-    cas_idx = jcamp_dict['cas registry no'].replace('-', '')
+        jcamp_dict = JCAMP_reader(os.path.join(mass_spec_path, jdx_file))
 
-    # Construct a temporary df for the molecule to store its x and y values
-    single_df = pd.DataFrame({'x': x_values, cas_idx: y_values})
-    single_df['x'] = pd.cut(single_df['x'], x_bins)
-    single_df = single_df.groupby('x').aggregate(np.mean).fillna(0)
+        x_values = jcamp_dict['x'] * float(jcamp_dict['xfactor'])
+        y_values = jcamp_dict['y'] * float(jcamp_dict['yfactor'])
 
-    plt.bar(list(map(str, single_df.index)), list(single_df[cas_idx]))
-    plt.xticks(rotation=90)
-    plt.tick_params(
-        axis='x',          # changes apply to the x-axis
-        which='both',      # both major and minor ticks are affected
-        bottom=False,      # ticks along the bottom edge are off
-        top=False,         # ticks along the top edge are off
-        labelbottom=False)  # labels along the bottom edge are off
-    plt.title(cas_idx)
-    plt.show()
+        # Scale by largest y val
+        y_values = y_values / max(y_values)
 
-    main_df = main_df.merge(single_df, on='x', how='outer')
+        cas_idx = jcamp_dict['cas registry no'].replace('-', '')
 
-pprint(main_df)
+        # Construct a temporary df for the molecule to store its x and y values
+        single_df = pd.DataFrame({'x': x_values, cas_idx: y_values})
+        single_df['x'] = pd.cut(single_df['x'], x_bins)
+        single_df = single_df.groupby('x').aggregate(np.mean).fillna(0)
+
+        main_df = main_df.merge(single_df, on='x', how='outer')
+
+    return main_df
+
+
+def main():
+    MASS_SPEC_PATH = os.path.join('data', 'mass_test')
+    mass_spec_df = create_mass_spec_df(MASS_SPEC_PATH)
+    pprint(mass_spec_df)
+
+    # Construct the output path for the dataframe
+    DF_PATH = os.path.join('data', 'MASS_SPEC_DF.pkl')
+
+    # Pickle the dataframe
+    mass_spec_df.to_pickle(DF_PATH)
+
+
+if __name__ == '__main__':
+    main()
