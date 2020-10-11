926b577262a585322082dff9b3715ec5b5c63249
Michae1 <333mmcc@gmail.com>
2020-10-12 07:53:22 +1000

    added graph MS

--- src_data/graph_MS.py
+++ src_data/graph_MS.py
@@ -6,18 +6,17 @@ import pandas as pd
 
 import matplotlib.pyplot as plt
 
-ir_data_path = os.path.join('data', 'IR_bins_test.csv')
-molecule_df = pd.read_csv(ir_data_path, index_col=0)
+ir_data_path = os.path.join('data', 'MASS_SPEC_DF.pkl')
+molecule_df = pd.read_pickle(ir_data_path)
 
-CAS_ID = '116668281'
 CAS_ID = '51456'
-# CAS_ID = '50022'
 
+pprint(molecule_df[CAS_ID])
 
-bins = molecule_df['x']
+bins = molecule_df.index
 ir_spec_data = molecule_df[CAS_ID]
 
-plt.plot(range(len(ir_spec_data)), ir_spec_data, color='red')
+plt.plot(range(len(ir_spec_data)), ir_spec_data, color='blue')
 plt.xticks(rotation=-20)
 plt.tick_params(
     axis='x',          # changes apply to the x-axis
@@ -25,7 +24,7 @@ plt.tick_params(
     bottom=False,      # ticks along the bottom edge are off
     top=False,         # ticks along the top edge are off
     labelbottom=False)  # labels along the bottom edge are off
-plt.xlabel("Wavenumber 1/CM")
-plt.ylabel("Transmittance")
-plt.title('IR spec for ' + CAS_ID)
+plt.xlabel("m/z")
+plt.ylabel("Relative Intensity")
+plt.title('MS spec for ' + CAS_ID)
 plt.show()
